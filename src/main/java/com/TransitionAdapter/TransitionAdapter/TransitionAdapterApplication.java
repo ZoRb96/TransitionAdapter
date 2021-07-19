6a945e51b72b8f1447463a1371d8ad322826fa61
ZoRb96 <sauravbhagabati@gmail.com>
2021-07-20 00:47:24 +0530

    Initial commit

---
+++ src/main/java/com/TransitionAdapter/TransitionAdapter/TransitionAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.TransitionAdapter.TransitionAdapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitionAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitionAdapterApplication.class, args);
+	}
+
+}
